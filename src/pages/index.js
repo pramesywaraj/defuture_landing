d0d524c9926516e3bb9a2c70af7e06c59bf7cb9e
Pramesywara Jembar P <pramesywara_jp@apps.ipb.ac.id>
2019-12-13 19:48:52 +0700

    add second page

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,10 +9,18 @@ import indexStyle from './styling/index.module.scss'
 const IndexPage = () => (
 	<Layout>
 		<SEO title="Home" />
-		<div className={indexStyle.cover}>
-			<h1>Genggam Dunia Digital Bersama Kami!</h1>
+		<section className={indexStyle.cover}>
+			<div className={indexStyle.title}>
+				<h1>Genggam Dunia Digital Bersama <font>Kami !</font></h1>
+				<p>Solusi untuk mengembangkan bisnis Anda</p>
+			</div>
 			{/* <img className={indexStyle.centeredImage} src={require('../assets/character_1.svg')} alt="Defuture Mascot"/> */}
-		</div>
+		</section>
+		<section className={indexStyle.about}>
+			<div>
+				<h1>Defuture Hadir!</h1>
+			</div>
+		</section>
 	</Layout>
 )
 
