a8b78d2aea9602a9c13b43718ba7af796ef899b5
Pramesywara Jembar P <pramesywara_jp@apps.ipb.ac.id>
2019-12-14 20:01:36 +0700

    about page almost complete

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,17 +23,17 @@ const IndexPage = () => (
 			<div className={indexStyle.aboutContainer}>
 				<div className={indexStyle.aboutBox}>
 					<div className={indexStyle.card}></div>
-					<p>Kami selalu memberikan saran!</p>
+					<p className={indexStyle.highlighted}>Kami selalu memberikan saran!</p>
 					<p>Kami tidak ingin pelanggan kami memiliki kendala pada saat ingin mewujudkan ide hebatnya.</p>
 				</div>
 				<div className={indexStyle.aboutBox}>
 					<div className={indexStyle.card}></div>
-					<p>Oh tidak, ada perubahan?!</p>
+					<p className={indexStyle.highlighted}>Oh tidak, ada perubahan?!</p>
 					<p>Tenang, kami siap dengan segala perubahan apabila tidak sesuai dan langsung mengerjakannya.</p>
 				</div>
 				<div className={indexStyle.aboutBox}>
 					<div className={indexStyle.card}></div>
-					<p>Para Client percaya dengan kami.</p>
+					<p className={indexStyle.highlighted}>Para Client percaya dengan kami.</p>
 					<p>Kami telah membantu Client kami dalam mewujudkan idenya bersama kami. Apakah Anda tahu? Mereka puas!</p>
 				</div>
 			</div>
